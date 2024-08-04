3e891c059002dcc2635aa69fd54d364b2ea2ff0b
Asroyx <mystoganza@hotmail.com>
2024-08-05 01:47:03 +0300

    refactor(directory): Files moved to new directory structure.

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let a = 15;
+
+  return (
+    <div>
+      <p>Value of variable a is: {a}</p>
+    </div>
+  );
+}
+
+export default App;
